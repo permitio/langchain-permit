cd47995878ae69ff247bb945b1326819e0051ac5
Taofiqq <taofiqaiyelabegan45@gmail.com>
2025-03-13 22:16:34 +0100

    refactor validator tool

--- langchain_permit/__init__.py
+++ langchain_permit/__init__.py
@@ -4,11 +4,11 @@ from importlib import metadata
 from langchain_permit.retrievers import PermitSelfQueryRetriever, PermitEnsembleRetriever
 from langchain_permit.tools import LangchainPermissionsCheckTool
 from langchain_permit.tools import LangchainJWTValidationTool
-
+# from langchain_permit.validator import JWTValidator
 
 try:
     # __version__ = metadata.version(__package__)
-     __version__ = metadata.version(__package__ or "langchain-permit")
+    __version__ = metadata.version(__package__ or "langchain-permit")
 except metadata.PackageNotFoundError:
     # Case where package metadata is not available.
     __version__ = "0.1.0"
@@ -19,5 +19,6 @@ __all__ = [
     "PermitEnsembleRetriever",
     "LangchainPermissionsCheckTool",
     "LangchainJWTValidationTool",
+    # "JWTValidator",
     "__version__",
 ]
